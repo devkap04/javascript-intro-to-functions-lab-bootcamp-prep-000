1e50bec2dea08eb3074ce8ce1435ecf6904d1ce0
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-06 07:53:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,5 @@ function sayHiToGrandma(string) {
   return "I can\'t hear you!";
   else if (string.toUpperCase() === string) 
   return "YES INDEED!";
+  else if (string === "I love you, Grandma.")
   
