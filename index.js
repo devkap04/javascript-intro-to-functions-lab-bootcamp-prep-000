3dc32e761a7d5a871d3020aaf0978d16ff9978d1
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-06 06:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  console.log()
-  return string.toLowerCase()
+  console.log(string.toLowerCase())
+  return 'I can\'t hear you!'
 }
